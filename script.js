42d4a18175c6df931d46117dafae5aa7f1c39b8f
imranZERO <imran.leave@gmail.com>
2023-08-21 12:37:51 +0600

    added kitchenware and coupon section

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log("YAYA");
+function getElementValueById(elementId) {
+	const element = document.getElementById(elementId);
+	const elementValueString = element.innerText;
+	const elementValue = parseFloat(elementValueString);
+	return elementValue;
+}
+
+function setElementValueById(elementId, newValue) {
+	const element = document.getElementById(elementId);
+	element.innerText = newValue.toFixed(2);
+}
+
+function enableBtn(currentTotal) {
+	// purchase button: enable by default
+	document.getElementById("btn-purchase").removeAttribute("disabled");
+	// discount apply button: enable if total price exceeds 200
+	if (currentTotal >= 200) {
+		document.getElementById("btn-apply").removeAttribute("disabled");
+	}
+}
+
+
+document.getElementById("k-accessories").addEventListener("click", function () {
+	const previousTotal = getElementValueById("total-price");
+	const currentTotal = previousTotal + 39;
+	setElementValueById("total-price", currentTotal);
+
+	const cartList = document.getElementById("cart-list");
+	const count = cartList.childElementCount;
+	const p = document.createElement("p");
+	p.innerText = `${count}. K. Accessories`;
+	cartList.appendChild(p);
+
+	enableBtn(currentTotal);
+})
