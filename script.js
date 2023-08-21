cb4224c19b312e9be184364882628aaa1e53cf2b
imranZERO <imran.leave@gmail.com>
2023-08-21 13:47:02 +0600

    added total and discount functionality

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Utility functions
 function getElementValueById(elementId) {
 	const element = document.getElementById(elementId);
 	const elementValueString = element.innerText;
@@ -19,17 +20,51 @@ function enableBtn(currentTotal) {
 	}
 }
 
-
-document.getElementById("k-accessories").addEventListener("click", function () {
+function updateTotals(plusValue) {
 	const previousTotal = getElementValueById("total-price");
-	const currentTotal = previousTotal + 39;
+	const currentTotal = previousTotal + plusValue;
 	setElementValueById("total-price", currentTotal);
 
+	const previousFinalTotal = getElementValueById("total-final");
+	const currentFinalTotal = previousFinalTotal + plusValue;
+	setElementValueById("total-final", currentFinalTotal);
+
+	enableBtn(currentTotal);
+}
+
+function appendToCart(productName) {
 	const cartList = document.getElementById("cart-list");
 	const count = cartList.childElementCount;
 	const p = document.createElement("p");
-	p.innerText = `${count}. K. Accessories`;
+	p.innerText = `${count + 1}. ${productName}`;
 	cartList.appendChild(p);
+}
 
-	enableBtn(currentTotal);
+// Coupon button
+document.getElementById("btn-apply").addEventListener("click", function () {
+	if (document.getElementById("coupon-code").value === "SELL200") {
+		const total = getElementValueById("total-price");
+		const discount = parseFloat((total * 0.2).toFixed(2));
+		setElementValueById("discount", discount);
+
+		let finalTotal = getElementValueById("total-final");
+		finalTotal = total - discount;
+		setElementValueById("total-final", finalTotal);
+	} else {
+		alert("Please enter the correct coupon code.");
+	}
+});
+
+// All product cards
+document.getElementById("k-accessories").addEventListener("click", function () {
+	updateTotals(39);
+	appendToCart("K. Accessories");
+})
+document.getElementById("k-set").addEventListener("click", function () {
+	updateTotals(25);
+	appendToCart("Kitchen Set");
+})
+document.getElementById("home-cooker").addEventListener("click", function () {
+	updateTotals(49);
+	appendToCart("Home Cooker");
 })
